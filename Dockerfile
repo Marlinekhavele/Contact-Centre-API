a2fac4b217c86abc8694aff3518631d91ccbe55f
Marline <marline@admins-air.speedport.ip>
2024-10-01 12:54:35 +0200

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,14 @@ FROM python:3.9
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-WORKDIR /chat
+WORKDIR /app
 
 # Install dependencies
-COPY requirements.txt /code/
+COPY requirements.txt /app/
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-COPY . /chat/
+COPY . /app/
 
 # Run the application
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "chat.wsgi:application"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- chat/settings.py
+++ chat/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
+import os
 from decouple import config, Csv
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
